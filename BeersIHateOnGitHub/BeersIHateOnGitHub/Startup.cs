30f28fda784b1e6693bb1d5159ed531107ae7007
wagner-mva <wagner100@comcast.net>
2015-06-23 11:19:42 -0700

    initial project code

---
+++ BeersIHateOnGitHub/BeersIHateOnGitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeersIHateOnGitHub.Startup))]
+namespace BeersIHateOnGitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
